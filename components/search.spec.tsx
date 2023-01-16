4bbe597b493cd0a6a8fa4d2823aaf2043dfb164e
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2023-01-16 20:21:45 -0300

    test: add tests on textInput in SearchComponent

---
+++ components/search.spec.tsx
@@ -0,0 +1,41 @@
+import Search from './search';
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const doSearch = jest.fn();
+describe('<Search />', () => {
+  it('Should render a form', async () => {
+    const { getByTestId, getByRole } = render(<Search doSearch={doSearch} />);
+    const form = getByRole('form', { name: 'search-form' });
+
+    expect(form).toBeInTheDocument();
+  });
+
+  it('Should call props.doSearch when form is submitted', async () => {
+    const { getByRole } = render(<Search doSearch={doSearch} />);
+    const form = getByRole('form', { name: 'search-form' });
+    await fireEvent.submit(form);
+    expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render a input type equals search', () => {
+    const { getByRole } = render(<Search doSearch={doSearch} />);
+    const input = getByRole('textbox', { name: 'text-search' });
+    expect(input).toHaveProperty('type', 'search');
+  });
+
+  it('Should call props.doSearch with the user input', async () => {
+    const { getByRole } = render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const form = getByRole('form', { name: 'search-form' });
+    const input = getByRole('textbox', { name: 'text-search' });
+
+    await userEvent.type(input, inputText);
+
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
+  });
+});
---
+++ components/search.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+type SearchProps = {
+  doSearch: (term: string) => void;
+};
+
+export default function Search({ doSearch }: SearchProps) {
+  const [term, setTerm] = useState('');
+
+  return (
+    <form
+      onSubmit={() => {
+        doSearch(term);
+      }}
+      aria-label="search-form"
+      role="form"
+      data-testid="search"
+      className="relative mt-6 max-w-lg mx-auto"
+    >
+      <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+        <svg className="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
+          <path
+            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
+
+      <input
+        role="textbox"
+        name="text-search"
+        aria-label="text-search"
+        onChange={(event) => setTerm(event.target.value)}
+        value={term}
+        type="search"
+        placeholder="Search"
+        className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
+      />
+    </form>
+  );
+}
