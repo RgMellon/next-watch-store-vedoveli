88803eb0d2af4e9ded004b8d7c03f1770ec540f2
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2023-01-25 08:56:30 -0300

    feat: add search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,37 @@
-import ProductCard from '../components/product-card';
+import { useFetchProducts } from '../hooks/use-fetch-products';
+import ProductCard, { ProdctItem } from '../components/product-card';
+import Search from '../components/search';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const { error, products } = useFetchProducts();
+  const [term, setTerm] = useState('');
+
+  // estados derivados
+  const filtredProduct =
+    term.length > 0
+      ? products.filter(
+          ({ title }) => title.toLocaleLowerCase().indexOf(term.toLocaleLowerCase()) > -1,
+        )
+      : products;
+
   return (
-    <main className="my-8">
+    <main className="my-8" data-testid="product-list">
+      <Search
+        doSearch={(term) => {
+          setTerm(term);
+        }}
+      />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          <ProductCard />
+          {!error && products.length === 0 && <h4 data-testid="no-products">No Products</h4>}
+          {error && <h4 data-testid="server-error">Server is down</h4>}
+
+          {filtredProduct.map((product) => (
+            <ProductCard key={product.id} product={product} addToCart={() => {}} />
+          ))}
         </div>
       </div>
     </main>
